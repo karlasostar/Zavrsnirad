aee12a109788a44a9d910855d6f08fe49535b04c
IvoGabud <76601963+IvoGabud@users.noreply.github.com>
2024-12-13 11:30:09 +0100

    prikaz podataka u tablici

---
+++ RPPP-WebApp/RPPP-WebApp/Controllers/DvoranaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RPPP_WebApp.Models;
+
+namespace RPPP_WebApp.Controllers
+{
+    public class DvoranaController : Controller
+    {
+        private readonly RPPP08Context ctx;
+        public DvoranaController(RPPP08Context ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var dvorane = ctx.Dvoranas.AsNoTracking().OrderBy(d => d.OznDvorana).ToList();
+            return View(dvorane);
+        }
+    }
+}
