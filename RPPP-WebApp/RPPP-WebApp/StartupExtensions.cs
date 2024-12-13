336b1b6abcb9be7c71e3c6df714745a77ae28c1b
Ivo Gabud <ivo.gabud@gmail.com>
2024-12-13 11:09:43 +0100

    Popravljen problem s kontekstom

--- RPPP-WebApp/RPPP-WebApp/StartupExtensions.cs
+++ RPPP-WebApp/RPPP-WebApp/StartupExtensions.cs
@@ -9,56 +9,55 @@ namespace RPPP_WebApp;
 
 public static class StartupExtensions
 {
-  public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-  {
-        IConfiguration configuration = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json")
-        .AddUserSecrets<Program>()
-         .Build();
-
-        IServiceCollection services = new ServiceCollection();
-        var provider = services
-        .AddDbContext<RPPP08Context>(options => {
-            options.UseSqlServer(
-            configuration.GetConnectionString("Server"));
-        }, contextLifetime: ServiceLifetime.Transient)
-        .BuildServiceProvider();
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<RPPP08Context>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Server")));
 
         builder.Services.AddControllersWithViews();
 
-
-        var serviceProvider = services.AddDbContext<RPPP08Context>().BuildServiceProvider();
-
-
-
         return builder.Build();
-  }
-
-  public static WebApplication ConfigurePipeline(this WebApplication app)
-  {
-    #region Needed for nginx and Kestrel (do not remove or change this region)
-    app.UseForwardedHeaders(new ForwardedHeadersOptions
-    {
-      ForwardedHeaders = ForwardedHeaders.XForwardedFor |
-                         ForwardedHeaders.XForwardedProto
-    });
-    string pathBase = app.Configuration["PathBase"];
-    if (!string.IsNullOrWhiteSpace(pathBase))
-    {
-      app.UsePathBase(pathBase);
-    }
-    #endregion
-
-    if (app.Environment.IsDevelopment())
-    {
-      app.UseDeveloperExceptionPage();
     }
 
-    app.UseStaticFiles()
-       .UseRouting();
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        #region Needed for nginx and Kestrel (do not remove or change this region)
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                             ForwardedHeaders.XForwardedProto
+        });
+        string pathBase = app.Configuration["PathBase"];
+        if (!string.IsNullOrWhiteSpace(pathBase))
+        {
+            app.UsePathBase(pathBase);
+        }
+        #endregion
 
-    app.MapDefaultControllerRoute();
+        //unos podataka u bazu test
 
-    return app;
-  }
+        //using (var scope = app.Services.CreateScope()) 
+        //{
+        //    var context = scope.ServiceProvider.GetRequiredService<RPPP08Context>();
+        //    Dvorana dvorana = new Dvorana
+        //    {
+        //        OznDvorana = "D2",
+        //        Kapacitet = 260
+        //    };
+        //    context.Dvoranas.Add(dvorana);
+        //    context.SaveChanges();
+        //}
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles()
+           .UseRouting();
+
+        app.MapDefaultControllerRoute();
+
+        return app;
+    }
 }
