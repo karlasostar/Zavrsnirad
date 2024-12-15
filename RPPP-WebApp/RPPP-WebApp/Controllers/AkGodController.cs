3b53fea79a0e27a8056a0ff42a6229ce68a0b710
Rene <rene.filipovic03@gmail.com>
2024-12-15 03:32:59 +0100

    dodana funkcionalnost dodavanja i brisanja nove akgod

---
+++ RPPP-WebApp/RPPP-WebApp/Controllers/AkGodController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPPP_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RPPP_WebApp.Controllers
+{
+    public class AkGodController : Controller
+    {
+        private readonly RPPP08Context context;
+
+        public AkGodController(RPPP08Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var akGodList = await context.Akgods.ToListAsync();
+
+            return View(akGodList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Akgod akgod)
+        {
+            var existingAkgod = await context.Akgods.FirstOrDefaultAsync(a => a.Razdoblje == akgod.Razdoblje);
+
+            if (existingAkgod != null)
+            {
+                ModelState.AddModelError("Razdoblje", "Ova akademska godina vec postoji!");
+                return View(akgod);
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Add(akgod);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(akgod);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var akgod = await context.Akgods.Include(a => a.Vijećes) .FirstOrDefaultAsync(a => a.IdAkGod == id);
+
+            if (akgod.Vijećes.Any())
+            {
+                TempData["ErrorMessage"] = "Ne mozete izbrisati akademsku godinu jer postoji vijece koje ju koristi.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (akgod != null)
+            {
+                context.Akgods.Remove(akgod);
+                context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ RPPP-WebApp/RPPP-WebApp/Controllers/VrstaVijecaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPPP_WebApp.Models;
+
+namespace RPPP_WebApp.Controllers
+{
+    public class VrstaVijecaController : Controller
+    {
+        private readonly RPPP08Context context;
+
+        public VrstaVijecaController(RPPP08Context context)
+        {
+            this.context = context;
+        }
+    }
+}
