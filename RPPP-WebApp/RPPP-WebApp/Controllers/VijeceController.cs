8b85a75b199ba9c60f7b48dfc694cc8ae4189bb7
Rene <rene.filipovic03@gmail.com>
2024-12-15 22:06:32 +0100

    dodan ispis mastera

--- RPPP-WebApp/RPPP-WebApp/Controllers/VijeceController.cs
+++ RPPP-WebApp/RPPP-WebApp/Controllers/VijeceController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RPPP_WebApp.Models;
+using RPPP_WebApp.ViewModels;
 
 namespace RPPP_WebApp.Controllers
 {
     public class VijeceController : Controller
     {
-        public IActionResult Index()
+        private readonly RPPP08Context context;
+
+        public VijeceController(RPPP08Context context)
         {
-            return View();
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vijeca = await context.Vijećes
+                .Include(v => v.IdAkGodNavigation) 
+                .Include(v => v.Fakultetsko) 
+                .Include(v => v.Strucno)   
+                .Include(v => v.Povjerentsvo)     
+                .Select(v => new VijeceSaSjednicom
+                {
+                    IdVijece = v.IdVijeca,
+                    Akgod = v.IdAkGodNavigation,
+                    TipVijeca =
+                        v.Fakultetsko != null ? "Fakultetsko" :
+                        v.Strucno != null ? "Stručno" :
+                        v.Povjerentsvo != null ? "Povjerenstvo" : "Nepoznato", 
+                    sjednice = v.Sjednicas 
+                })
+                .ToListAsync();
+            /*
+            var zavrsniRadWithDecisions = vijeca
+                .Select(v => new VijeceSaSjednicom
+                {
+                    
+                }).ToList();
+            */
+
+            var model = new VijeceViewModel
+            {
+                Vijece = vijeca
+            };
+
+            return View(model);
         }
     }
 }
---
+++ RPPP-WebApp/RPPP-WebApp/ViewModels/VijeceViewModel.cs
@@ -0,0 +1,24 @@
+﻿using RPPP_WebApp.Models;
+
+namespace RPPP_WebApp.ViewModels
+{
+    public class VijeceViewModel
+    {
+        public IEnumerable<VijeceSaSjednicom> Vijece { get; set; }
+    }
+
+    public class VijeceSaSjednicom
+    {
+        public int IdVijece { get; set; }
+        public string TipVijeca { get; set; }
+        public virtual ICollection<ZavrsniRad> ZavrsniRadovi { get; set; } = new List<ZavrsniRad>();
+        public virtual Vijeće StrucnoVijece { get; set; }
+        public virtual ICollection<Vijeće> Povjerenstva { get; set; } = new List<Vijeće>();
+        public virtual ICollection<OdlukeFv> OdlukeFvs { get; set; } = new List<OdlukeFv>();
+        public virtual Akgod Akgod { get; set; }
+        public virtual Akgod IdAkGodNavigation { get; set; }
+        public virtual ICollection<VijeceUlogaZap> VijeceUlogaZaps { get; set; } = new List<VijeceUlogaZap>();
+        public virtual ICollection<Sjednica> sjednice { get; set; } = new List<Sjednica>();
+
+    }
+}
