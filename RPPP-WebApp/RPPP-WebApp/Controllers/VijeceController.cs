239db1eda437894cc3f297bcd07848226a34b673
Rene <rene.filipovic03@gmail.com>
2024-12-16 03:55:35 +0100

    dodano sortiranje i paginacija na index mastera

--- RPPP-WebApp/RPPP-WebApp/Controllers/VijeceController.cs
+++ RPPP-WebApp/RPPP-WebApp/Controllers/VijeceController.cs
@@ -15,49 +15,18 @@ namespace RPPP_WebApp.Controllers
             this.context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, int? pageNumber)
         {
-            /*
+
             ViewData["IdSortParam"] = String.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
             ViewData["NazivSortParam"] = sortOrder == "Naziv" ? "naziv_desc" : "Naziv";
             ViewData["CurrentSort"] = sortOrder;
 
-            var vijece = from v in context.Vijećes select v;
-
-            switch (sortOrder)
-            {
-                case "id_desc":
-                    vijece = vijece.OrderByDescending(v => v.IdVijeca);
-                    break;
-                case "Naziv":
-                    vijece = vijece.OrderBy(v => v.Razdoblje == null ? "" : v.Razdoblje.ToUpper());
-
-                    break;
-                case "naziv_desc":
-                    vijece = vijece.OrderByDescending(v => v.Razdoblje == null ? "" : v.Razdoblje.ToUpper());
-                    break;
-                default:
-                    akgod = akgod.OrderBy(v => v.Id);
-                    break;
-            }
-
-            int pageSize = 5;
-            int currentPage = pageNumber ?? 1;
-            int totalRecords = await akgod.CountAsync();
-
-            var pagedData = await akgod.Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
-
-            ViewData["CurrentPage"] = currentPage;
-            ViewData["TotalPages"] = (int)Math.Ceiling(totalRecords / (double)pageSize);
-
-            return View(pagedData);
-            */
-
-            var vijeca = await context.Vijećes
-                .Include(v => v.IdAkGodNavigation) 
+            var vijece = await context.Vijećes
+                .Include(v => v.IdAkGodNavigation)
                 .Include(v => v.Sjednicas)
-                .Include(v => v.Fakultetsko) 
-                .Include(v => v.Strucno)   
+                .Include(v => v.Fakultetsko)
+                .Include(v => v.Strucno)
                 .Include(v => v.Povjerentsvo)
                 .Select(v => new VijeceSaSjednicom
                 {
@@ -66,26 +35,39 @@ namespace RPPP_WebApp.Controllers
                     TipVijeca =
                         v.Fakultetsko != null ? "Fakultetsko" :
                         v.Strucno != null ? "Stručno" :
-                        v.Povjerentsvo != null ? "Povjerenstvo" : "Nepoznato", 
+                        v.Povjerentsvo != null ? "Povjerenstvo" : "Nepoznato",
                     sjednice = v.Sjednicas,
                     Rbr = v.Sjednicas.OrderByDescending(s => s.RBr)
                     .FirstOrDefault().RBr
                 })
                 .ToListAsync();
-            /*
-            var zavrsniRadWithDecisions = vijeca
-                .Select(v => new VijeceSaSjednicom
-                {
-                    
-                }).ToList();
-            */
+
+            switch (sortOrder)
+            {
+                case "id_desc":
+                    vijece = vijece.OrderByDescending(v => v.IdVijece).ToList();
+                    break;
+                default:
+                    vijece = vijece.OrderBy(v => v.IdVijece).ToList();
+                    break;
+            }
+
+            int pageSize = 5;
+            int currentPage = pageNumber ?? 1;
+            int totalRecords = vijece.Count;
+
+            var pagedData = vijece.Skip((currentPage - 1) * pageSize).Take(pageSize);
+
+            ViewData["CurrentPage"] = currentPage;
+            ViewData["TotalPages"] = (int)Math.Ceiling(totalRecords / (double)pageSize);
 
             var model = new VijeceViewModel
             {
-                Vijece = vijeca
+                Vijece = vijece
             };
 
             return View(model);
+
         }
 
     }
