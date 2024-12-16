3e0d56e6c6b825e6c7e2588948a84e0dd1c8c9ec
Ivo Gabud <ivo.gabud@gmail.com>
2024-12-16 05:00:03 +0100

    Update StartupExtensions.cs

--- RPPP-WebApp/RPPP-WebApp/StartupExtensions.cs
+++ RPPP-WebApp/RPPP-WebApp/StartupExtensions.cs
@@ -57,7 +57,7 @@ public static class StartupExtensions
         app.UseStaticFiles()
            .UseRouting();
 
-        app.MapDefaultControllerRoute();
+        
 
         app.MapControllerRoute(
             name: "dvorana",
